d0b5e24b12964f1869d09c05a54f5cca61d7f7bc
Ronnie <ronniedipple@gmail.com>
2020-03-11 17:33:48 -0600

    last block end point working

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -4,7 +4,7 @@ from time import time
 from uuid import uuid4
 
 from flask import Flask, jsonify, request
-
+#tutorial https://www.youtube.com/watch?v=iRZBPDZ71Ak&feature=youtu.be
 
 class Blockchain(object):
     def __init__(self):
@@ -89,17 +89,24 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
-        # TODO
-        pass
-        # return proof
+    # def proof_of_work(self, block):
+    #     """
+    #     Simple Proof of Work Algorithm
+    #     Stringify the block and look for a proof.
+    #     """
+    #     block_string = json.dumps(block, sort_keys=True)
+    # 
+    #     """
+    #     Loop through possibilities, checking each one against `valid_proof`
+    #     in an effort to find a number that is a valid proof
+    #     :return: A valid proof for the provided block
+    #     """
+    # 
+    #     proof = 0
+    #     while self.valid_proof(block_string, proof) is False:
+    #        proof +=1
+    #     return proof
+    #     # return proof
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -113,8 +120,9 @@ class Blockchain(object):
         correct number of leading zeroes.
         :return: True if the resulting hash is a valid proof, False otherwise
         """
-        # TODO
-        pass
+        guess = f'{block_string}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:6] == "000000" #increase this to increase mining time aka finding the hash
         # return True or False
 
 
@@ -131,11 +139,23 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
+    proof = blockchain.proof_of_work(blockchain.last_block)
 
     # Forge the new Block by adding it to the chain with the proof
+    previous_hash = blockchain.hash(blockchain.last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
 
     response = {
-        # TODO: Send a JSON response with the new block
+        'new_block': block
+    }
+
+    return jsonify(response), 200
+
+@app.route('/last_block', methods=['GET'])
+def last_block ():
+    response = {
+        'last_block': blockchain.last_block
     }
 
     return jsonify(response), 200
@@ -154,3 +174,45 @@ def full_chain():
 # Run the program on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
+    
+# Client Miners
+
+# In the initial blockchain demonstration, we've created a small problem.  
+# The `mine` endpoint is called on the server, 
+# which means we're the ones spending all of the electricity to generate a new block.  
+# This won't do at all!
+# 
+# Furthermore, the amount of work needed to actually mine a block is a bit low.  
+# We need it to be harder to preserve the integrity of the chain.
+
+
+# Task List
+
+# *Server*
+# Modify the server we created to:
+# * Remove the `proof_of_work` function from the server.
+# * Change `valid_proof` to require *6* leading zeroes.
+# * Add an endpoint called `last_block` that returns the last block in the chain
+# * Modify the `mine` endpoint to instead receive and validate or reject a new proof sent by a client.
+#     * It should accept a POST
+#     * Use `data = request.get_json()` to pull the data out of the POST
+#         * Note that `request` and `requests` both exist in this project
+#     * Check that 'proof', and 'id' are present
+#         * return a 400 error using `jsonify(response)` with a 'message'
+# * Return a message indicating success or failure.  
+# Remember, a valid proof should fail for all senders except the first.
+# 
+# *Client Mining*
+# Create a client application that will:
+# * Get the last block from the server
+# * Run the `proof_of_work` function until a valid proof is found, validating or rejecting each attempt.  
+# Use a copy of `valid_proof` to assist.
+# * Print messages indicating that this has started and finished.
+# * Modify it to generate proofs with *6* leading zeroes.
+# * Print a message indicating the success or failure response from the server
+# * Add any coins granted to a simple integer total, and print the amount of coins the client has earned
+# * Continue mining until the app is interrupted.
+# * Change the name in `my_id.txt` to your name
+# * (Stretch) Handle non-json responses sent by the server in the event of an error, without crashing the miner
+# * Stretch: Add a timer to keep track of how long it takes to find a proof
