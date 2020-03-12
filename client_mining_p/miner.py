30166cf8130e906c41567f7035b679dae4e39a2b
Ronnie <ronniedipple@gmail.com>
2020-03-11 20:44:10 -0600

    mvp

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,20 +5,30 @@ import sys
 import json
 
 
-def proof_of_work(block):
+def proof_of_work(last_block):
     """
     Simple Proof of Work Algorithm
     Stringify the block and look for a proof.
+    """
+    block_string = json.dumps(last_block, sort_keys=True)
+
+    """
     Loop through possibilities, checking each one against `valid_proof`
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
+    # return proof
+
 
 
 def valid_proof(block_string, proof):
     """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
+    Validates the Proof:  Does hash(block_string + proof) contain 3
     leading zeroes?  Return true if the proof is valid
     :param block_string: <string> The stringified block to use to
     check in combination with `proof`
@@ -27,7 +37,10 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"  # increase this to increase mining time aka finding the hash
+    # return True or False
 
 
 if __name__ == '__main__':
@@ -42,7 +55,7 @@ if __name__ == '__main__':
     id = f.read()
     print("ID is", id)
     f.close()
-
+    coins_mined = 0 # Placed by me so it does not reset to 0
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -57,14 +70,26 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        new_proof = proof_of_work(data['last_block'])
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
+        try:
+            data = r.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(r)
+            break
 
-        # TODO: If the server responds with a 'message' 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        #  If the server responds with a 'message' 'New Block Forged'
+        # add 1 to the number of coins mined and print it.  variable made above
+
+        if data['message'] == 'Yay you mined/forged a new block':
+            coins_mined += 1
+            print(f'{coins_mined} coins mined')
+        else:
+            # print the message from the server.
+            print(data['message'])
